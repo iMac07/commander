570b6fbeaa4afbe505ec718f6e0f8f9a66bf6566
Michael Cuison <xurpas7@gmail.com>
2021-07-21 00:08:22 +0800

    20210720

---
+++ src/org/xersys/commander/iface/XRecord.java
@@ -0,0 +1,23 @@
+package org.xersys.commander.iface;
+
+public interface XRecord {
+    boolean NewRecord();
+    boolean NewRecord(String fsTmpTrans);
+    boolean SaveRecord();
+    boolean UpdateRecord();
+    boolean OpenRecord(String fsTransNox);
+    boolean DeleteRecord(String fsTransNox);
+    boolean DeactivateRecord(String fsTransNox);
+    boolean ActivateRecord(String fsTransNox);
+
+    String getMessage();
+   
+    void setMaster(String fsFieldNm, Object foValue);
+    Object getMaster(String fsFieldNm);
+
+    void setMaster(int fnIndex, Object foValue);
+    Object getMaster(int fnIndex);
+   
+    void setListener(Object foListener);
+    void setSaveToDisk(boolean fbValue);
+}
