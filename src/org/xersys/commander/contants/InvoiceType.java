d6576ba1bd44c2f056e3f340e50c55b390e75fa1
Michael Cuison <xurpas7@gmail.com>
2021-09-15 23:44:55 +0800

    20210915

--- src/org/xersys/commander/contants/InvoiceType.java
+++ src/org/xersys/commander/contants/InvoiceType.java
@@ -3,4 +3,5 @@ package org.xersys.commander.contants;
 public class InvoiceType {
     public static final String SALES_INVOICE = "0";
     public static final String OFFICIAL_RECEIPT = "1";
+    public static final String NO_INVOICE = "2";
 }
