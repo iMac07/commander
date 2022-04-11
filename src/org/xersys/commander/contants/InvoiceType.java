64384cb53de1b341b57e6a5c04a7e2f8fd56426b
daedalus07 <xurpas.daedalus@gmail.com>
2022-04-12 00:39:47 +0800

    20220412

--- src/org/xersys/commander/contants/InvoiceType.java
+++ src/org/xersys/commander/contants/InvoiceType.java
@@ -5,4 +5,5 @@ public class InvoiceType {
     public static final String OFFICIAL_RECEIPT = "1";
     public static final String NO_INVOICE = "2";
     public static final String CHARGE_INVOICE = "3";
+    public static final String OR_SI = "4";
 }
